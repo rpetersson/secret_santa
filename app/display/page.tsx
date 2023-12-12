55ba579cd5dba4e493d642cac9d3c746bfdaef57
Robert Petersson <rpetersson@gmail.com>
2023-12-12 22:08:00 +0100

    Co-authored-by: tenrecc <tenrecc@users.noreply.github.com>

---
+++ app/display/page.tsx
@@ -0,0 +1,9 @@
+export default function display() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>display</h1>
+        
+      </main>
+    )
+  }
+  
---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+export default function login() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>login</h1>
+        
+      </main>
+    )
+  }
+  
