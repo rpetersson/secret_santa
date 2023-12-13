3debbeb27e5acb0aa9cca83d7fa6eecbce7f9509
taulex <alelin702@gmail.com>
2023-12-13 22:02:45 +0100

    bakgrund

--- app/page.tsx
+++ app/page.tsx
@@ -18,29 +18,32 @@ export default function Home() {
       <img src="/secret_santa.png" alt="Secret Santa Logo" className="w-1/5" />
 
       <video autoPlay muted loop id="myVideo">
-      <source src="snowfall.mp4" type="video/mp4"></source>
-      Your browser does not support HTML5 video.
+        <source src="snowfall.mp4" type="video/mp4"></source>
+        Your browser does not support HTML5 video.
       </video>
-
-      <h1>Welcome to secret santa!</h1>
-      <h2>Enter the names of the players below!</h2>
-
-      <form action={submitName} className="flex flex-col items-center justify-between">
-        <input className="text-black" type="text" placeholder="Name" id="name_input_field"></input>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Lägg till!</button>
-      </form>
       
-      <p>Players:</p>
-      <div className='players'>
-        <ul id='player_list'>
+      <div className='hej'>
+        <h1>Welcome to secret santa!</h1>
+        <h2>Enter the names of the players below!</h2>
 
-        </ul>
+        <form action={submitName} className="flex flex-col items-center justify-between">
+          <input className="text-black" type="text" placeholder="Name" id="name_input_field"></input>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Lägg till!</button>
+        </form>
+
+        <p>Players:</p>
+        <div className='players'>
+          <ul id='player_list'>
+
+          </ul>
+        </div>
+        <Link className="py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 rounded" href="/generate">GoGo Secret santa -{">"}</Link>
+
+        <form action={dropAllEntities}>
+          <button className="py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 rounded" >Ta bort alla namn!</button>
+        </form>
+
       </div>
-      <Link className="py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 rounded" href="/generate">GoGo Secret santa -{">"}</Link>
-
-      <form action={dropAllEntities}>
-        <button className="py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 rounded" >Ta bort alla namn!</button>
-      </form>
-      </>
+    </>
   )
 }
