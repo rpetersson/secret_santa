acc522a9cf5f04bc532908b18b90cb809cea4456
Robert Petersson <rpetersson@gmail.com>
2023-12-13 21:34:38 +0100

    Co-authored-by: taulex <taulex@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <img src="/secret_santa.png" alt="Secret Santa Logo" className="w-1/5" />
 
       <video autoPlay muted loop id="myVideo">
-      <source src="snowfall.mp4" type="video/mp4"></source>
+      <source src="test.mp4" type="video/mp4"></source>
       Your browser does not support HTML5 video.
       </video>
 
