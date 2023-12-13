1128177a6ab19724554c0b0d7a47c405a38447c2
taulex <alelin702@gmail.com>
2023-12-13 22:28:47 +0100

    knappar centrerade

--- app/page.tsx
+++ app/page.tsx
@@ -19,26 +19,28 @@ export default function Home() {
         <source src="snowfall.mp4" type="video/mp4"></source>
         Your browser does not support HTML5 video.
       </video>
-      
-      <div className='hej'>
+
+      <div className="hej">
         <h1>Welcome to secret santa!</h1>
         <h2>Enter the names of the players below!</h2>
 
         <form action={submitName} className="flex flex-col items-center justify-between">
           <input className="text-black" type="text" placeholder="Name" id="name_input_field"></input>
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Lägg till!</button>
+
         </form>
 
         <p>Players:</p>
-        <div className='players'>
+        <div className="m-3">
           <ul id='player_list'>
 
           </ul>
         </div>
-        <Link className="py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 rounded" href="/generate">GoGo Secret santa -{">"}</Link>
-
-        <form action={dropAllEntities}>
-          <button className="py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 rounded" >Ta bort alla namn!</button>
+        <div className="flex flex-col items-center justify-between">
+          <Link className=" py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 rounded " href="/generate">GoGo Secret santa -{">"}</Link>
+        </div>
+        <form action={dropAllEntities} className="flex flex-col items-center justify-between">
+          <button className="m-3 py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 rounded " >Ta bort alla namn!</button>
         </form>
 
       </div>
